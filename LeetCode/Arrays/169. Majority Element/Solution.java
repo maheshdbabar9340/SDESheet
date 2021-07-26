3cb8dfe945b19e89d7f4ae05575aaae279138f2e
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2021-07-26 18:11:22 +0530

    Update Solution.java

--- LeetCode/Arrays/169. Majority Element/Solution.java
+++ LeetCode/Arrays/169. Majority Element/Solution.java
@@ -1,18 +1,37 @@
-// O(n) time complextiy
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         if(nums.length ==1) {
+//             return nums[0];
+//         }
+        
+//         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+//         for(int i : nums) {
+//             if(map.containsKey(i) && map.get(i) + 1 > nums.length /2 ) {
+//                 return i;
+//             } else{
+//                 map.put(i, map.getOrDefault(i, 0) + 1);
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        if(nums.length ==1) {
-            return nums[0];
-        }
+        int count = 0;
+        int index = 0;
         
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i : nums) {
-            if(map.containsKey(i) && map.get(i) + 1 > nums.length /2 ) {
-                return i;
-            } else{
-                map.put(i, map.getOrDefault(i, 0) + 1);
+        for(int i : nums){
+            if(count==0) {
+                index = i;
+            }
+            
+            if(i == index) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return -1;
+        return index;
     }
 }
